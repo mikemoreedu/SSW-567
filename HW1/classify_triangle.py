d006dfef4a19517a68a5fb548368f0128a8e6087
mikemoreedu <81551332+mikemoreedu@users.noreply.github.com>
2022-12-21 23:47:19 -0500

    updates

--- HW1/classify_triangle.py
+++ HW1/classify_triangle.py
@@ -1,19 +1,20 @@
-a = int(input("Enter side length a: "))
-b = int(input("Enter side length b: "))
-c = int(input("Enter side length c: "))
+"""
+takes args a, b, c and classifies them as a type of triangle, or not a triangle"""
+side_a = int(input("Enter side length a: "))
+side_b = int(input("Enter side length b: "))
+side_c = int(input("Enter side length c: "))
 
-def classify_triangle(a,b,c):
-    if a <= 0 or b <= 0 or c <= 0 or a + b < c or a + c < b or b + c < a:
-        return "Not a Triangle" 
-    else:
-        if a == b and b == c:
-            return "Equilateral"
-        elif a == b or a == c or b == c:
-            return "Isosceles"
-        elif a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2:
-            return "Scalene Right"
-        else:
-            return "Scalene"
+def classify_triangle(side_a,side_b,side_c):
+    """check if triangle is valid first, then check for each type of triangle and right triangles"""
+    if side_a <= 0 or side_b <= 0 or side_c <= 0 or side_a + side_b < side_c or side_a + side_c < side_b or side_b + side_c < side_a:
+        return "Not a Triangle"
+    if side_a == side_b and side_b == side_c:
+        return "Equilateral"
+    if side_a == side_b or side_a == side_c or side_b == side_c:
+        return "Isosceles"
+    if side_a**2 + side_b**2 == side_c**2 or side_a**2 + side_c**2 == side_b**2 or side_b**2 + side_c**2 == side_a**2:
+        return "Scalene Right"
+    return "Scalene"
 #also comment the following two lines when testing
-classify_triangle(a, b, c)
-print(classify_triangle(a,b,c))
+classify_triangle(side_a,side_b,side_c)
+print(classify_triangle(side_a,side_b,side_c))
--- HW1/classify_triangle_test.py
+++ HW1/classify_triangle_test.py
@@ -1,14 +1,14 @@
-#performs various tests on classify_triangle.py
+"""#performs various tests on classify_triangle.py"""
 from classify_triangle import classify_triangle
 
-def test_classify_triangle(): #testing various cases on classify_triangle()
-    
-    assert classify_triangle(3, 4, 5) == "Scalene Right"
-    assert classify_triangle(5, 5, 5) == "Equilateral"
-    assert classify_triangle(5, 5, 7) == "Isosceles"
-    assert classify_triangle(3, 3, 4) == "Isosceles"
-    assert classify_triangle(2, 3, 4) == "Scalene"
-    assert classify_triangle(5, 7, 9) == "Scalene"
-    assert classify_triangle(8, 15, 17) == "Scalene Right"
-    assert classify_triangle(0, 0, 0,) == "Not a Triangle"
-    assert classify_triangle(1, 2, 5,) == "Not a Triangle"
+def test_classify_triangle(): """testing various cases on classify_triangle()"""
+
+assert classify_triangle(3, 4, 5) == "Scalene Right"
+assert classify_triangle(5, 5, 5) == "Equilateral"
+assert classify_triangle(5, 5, 7) == "Isosceles"
+assert classify_triangle(3, 3, 4) == "Isosceles"
+assert classify_triangle(2, 3, 4) == "Scalene"
+assert classify_triangle(5, 7, 9) == "Scalene"
+assert classify_triangle(8, 15, 17) == "Scalene Right"
+assert classify_triangle(0, 0, 0,) == "Not a Triangle"
+assert classify_triangle(1, 2, 5,) == "Not a Triangle"
