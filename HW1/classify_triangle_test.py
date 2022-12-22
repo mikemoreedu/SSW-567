fb834fa0d0ccc890b42bc1e704108b580d78dee3
mikemoreedu <81551332+mikemoreedu@users.noreply.github.com>
2022-12-21 22:40:25 -0500

    Create classify_triangle_test.py

---
+++ HW1/classify_triangle_test.py
@@ -0,0 +1,14 @@
+import pytest
+from classify_triangle import classify_triangle
+
+def test_classify_triangle():
+    assert classify_triangle(3, 4, 5) == "Scalene Right"
+    assert classify_triangle(5, 5, 5) == "Equilateral"
+    assert classify_triangle(5, 5, 7) == "Isosceles"
+    assert classify_triangle(3, 3, 4) == "Isosceles"
+    assert classify_triangle(2, 3, 4) == "Scalene"
+    assert classify_triangle(5, 7, 9) == "Scalene"
+    assert classify_triangle(8, 15, 17) == "Scalene Right"
+    assert classify_triangle(0, 0, 0,) == "Not a Triangle"
+    assert classify_triangle(1, 2, 5,) == "Not a Triangle"
+
